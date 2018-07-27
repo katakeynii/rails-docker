1c7b5a45dc8457fa7d985f62b7408a91b1fc89b4
Elias Waly BA <eliaswalyba@gmail.com>
2018-07-27 17:55:20 +0000

    updating the compose file

--- init.sh
+++ init.sh
@@ -10,6 +10,8 @@ echo "-"
 sudo chmod 755 -R .	
 sudo docker-compose build 
 sudo docker-compose run app rails new . -d postgresql
+echo "------------------------------------"
+echo "------------------------------------"
 sudo mv default.database.yml ./config/database.yml
 # sudo docker-compose run app rails db:create db:migrate 
 sudo docker-compose up
