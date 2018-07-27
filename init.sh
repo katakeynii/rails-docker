810e7f11229d97bad4d16ce67d6b15aabb84a566
Elias Waly BA <eliaswalyba@gmail.com>
2018-07-27 17:37:52 +0000

    addding init script and bundle install script

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "=================================================="
+echo "============= STARTING RAILS APP ================="
+echo "=================================================="
+echo "-"
+echo "-"
+
+sudo chmod 755 -R .	
+sudo docker-compose build 
+sudo docker-compose app rails new . -d postgresql
+sudo mv default.database.yml ./config/database.yml
+# sudo docker-compose run app rails db:create db:migrate 
+sudo docker-compose up
+# sudo chown -R $USER:$USER .
