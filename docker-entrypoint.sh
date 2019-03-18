754ff879ed0fa3d69dd82c7bcd56973a5a8a0c14
Mohamed Camara GUEYE <mohamed.gueye@akilee-by-ines.com>
2019-03-18 12:28:04 +0000

    update entrypoint.sh and compose file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,13 @@ if [ -f tmp/pids/server.pid ]; then
   rm -rf tmp/pids/server.pid
 fi
 
+bundle check || bundle install --binstubs="$BUNDLE_BIN"
+# Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
+
+# bundle exec rails s -b 0.0.0.0
+
+
+exec "$@"
+# exec bundle exec "$@"
+
 # exec bundle exec "$@"
