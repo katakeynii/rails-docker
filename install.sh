ecc96a150eb4d1a222f07b4d138e0d6f1abd17ba
Mohamed Camara Gueye <katakeynii@gmail.com>
2021-11-16 22:55:18 +0000

    udpatinginstall

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ echo "${green}=================================================="
 echo "${green}===== GENERATING A NEW RAILS APP ================="
 echo "${green}=================================================="
 
+mv .env.default .env
 docker-compose build
 docker-compose run app rails new . -d postgresql
 
